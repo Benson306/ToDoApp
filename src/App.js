8a2f348ba2205e8f3ea7d0adc5403ba22846561c
Benson Ndiwa <bnkimtai@gmail.com>
2023-02-08 16:50:03 +0300

    Toastify Done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import Ribbon from "./components/Ribbon";
 import Tasks from "./components/Tasks";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
 
 function App() {
   return (
     <div className="App bg-[url('./assets/mount2.jpg')] bg-cover  min-h-screen">
+      <ToastContainer />
       <Ribbon />
       <Tasks />
     </div>
